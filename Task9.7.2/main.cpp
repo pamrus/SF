a45a5a8b9131c65fe3c1bdd14ad2c3e368bac988
pamrus <90761623+pamrus@users.noreply.github.com>
2021-09-29 15:10:58 +0500

    Add files via upload

---
+++ Task9.7.2/main.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+char* foo(int size)
+{
+    char* result = NULL;
+    if (size > 0) {
+        result = (char*)malloc(size * sizeof(char));
+    }
+    if (size > 10)
+    {
+        free(result);
+        return NULL;
+    }
+    return result;
+}
+int main()
+{
+    int i;
+    char* ptr;
+    cin >> i;
+    ptr = foo(i);
+    if (i > 0 && ptr)
+    {
+        free(ptr);
+    }
+}
